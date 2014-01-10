827cfcded04338bcc5a87faf3f2577e9d1d94503
Jan Järfalk <jan@jarfalk.se>
2014-01-10 22:58:38 +0100

    Refactored monogram. Started timeline.

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-var hue = Math.round(Math.random()*360);
-var hsl = [hue, '74%', 60];
-var backgroundColor = 'hsl(' + [hue, '74%', 80].join(',') + '%)';
-
-var shades = [];
-for (var i = 0; i < 10; i++) {
+var createHues = function(){
     var ratio = ((1+Math.sqrt(5))/2);
-    var darken = Math.pow(i,ratio);
-    var color = hsl;
-        color[2] = Math.round( color[2] * (100-darken)/100);
-    shades.push( 'hsl(' + color.join(',') + '%)' );
+    var hues = [];
+    for(var j = 0; j < 360; j++){
+        var shades = [];
+        var hsl = [j, '74%', 60];
+        for (var i = 0; i <= 9; i++) {
+            var color = hsl;
+            color[2] = Math.round( color[2] * (100-Math.pow(i,ratio))/100);
+            shades.push( 'hsl(' + color.join(',') + '%)' );
+        };
+        hues.push(shades);
+    }
+    return hues;
+}
+
+var setColour = function( hue ){
+
+    // Set background color.
+    var backgroundColor = hues[hue][9];
+    document.getElementById('hero').style.backgroundColor = backgroundColor;
+
+    // Set polygon colors.
+    var elements = document.querySelectorAll('[data-shade]');
+    for (var i = elements.length - 1; i >= 0; i--) {
+        var shadeIndex = elements[i].getAttribute('data-shade');
+        elements[i].setAttribute('fill', hues[hue][shadeIndex]);
+    }
+
 };
 
-var elements = document.querySelectorAll('[data-shade]');
+var hues = createHues();
 
-for (var i = elements.length - 1; i >= 0; i--) {
-    var shadeIndex = elements[i].getAttribute('data-shade');
-    elements[i].setAttribute('fill', shades[shadeIndex]);
-};
+// Set starting hue.
+var hue = Math.floor(Math.random()*360);
+setColour(hue);
 
-document.getElementById('hero').style.backgroundColor = backgroundColor;
-setTimeout(function(){
+// Change hue every 300ms.
+setInterval(function(){
     document.getElementById('hero').style.height = parent.document.body.clientHeight + 'px';
-    console.log(document.documentElement.clientHeight, parent.document.body.clientHeight);
-},1);
+    if( ++hue >= 360 ) hue = 0;
+    setColour(hue);
+},300);
